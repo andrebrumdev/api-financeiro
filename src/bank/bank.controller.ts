e984cc88eaf0f308c6e478ca77ccf65d1204c7d5
Andre <andrebrum.dev@gmail.com>
2023-06-21 14:57:33 -0400

    all banks

--- src/bank/bank.controller.ts
+++ src/bank/bank.controller.ts
@@ -6,31 +6,39 @@ import { join } from 'path';
 
 @Controller('bank')
 export class BankController {
-  constructor(
-    private readonly bankService: BankService,
-    private readonly repository: Repository
-) {}
+    constructor(
+        private readonly bankService: BankService,
+        private readonly repository: Repository
+    ) { }
 
-  @Get(':id/icon')
-  @Header('Content-Type','image/*')
-  getBankIcon(@Param('id') id: string, @Res({ passthrough: true }) res): StreamableFile {
-    const bankId = String(Number(id)).padStart(3, '0');
-    const imagePath = `/src/database/img/${bankId}.jpg`;
-    const file = createReadStream(join(process.cwd(), imagePath));
-    res.set({
-        'Content-Disposition': `attachment; filename="${bankId}.jpg"`,
-    });
-    return new StreamableFile(file);
-  }
+    @Get('')
+    async getBankAll() {
+        const bancosJSON = await this.bankService.local();
+        return {
+            status: 200,
+            json:bancosJSON,
+        };
+    }
+    @Get(':id')
+    async getBankDetails(@Param('id') id: string) {
+        const bankId = String(Number(id)).padStart(3, '0');
+        const bancosJSON = await this.bankService.local();
+        const json = await this.repository.findById(bancosJSON, bankId);
+        return {
+            status: 200,
+            json,
+        };
+    }
+    @Get(':id/icon')
+    @Header('Content-Type', 'image/*')
+    getBankIcon(@Param('id') id: string, @Res({ passthrough: true }) res): StreamableFile {
+        const bankId = String(Number(id)).padStart(3, '0');
+        const imagePath = `/src/database/img/${bankId}.jpg`;
+        const file = createReadStream(join(process.cwd(), imagePath));
+        res.set({
+            'Content-Disposition': `attachment; filename="${bankId}.jpg"`,
+        });
+        return new StreamableFile(file);
+    }
 
-  @Get(':id')
-  async getBankDetails(@Param('id') id: string) {
-    const bankId = String(Number(id)).padStart(3, '0');
-    const bancosJSON = await this.bankService.local();
-    const json = await this.repository.findById(bancosJSON, bankId);
-    return {
-      status: 200,
-      json,
-    };
-  }
 }
